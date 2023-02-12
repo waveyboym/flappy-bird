ffdfd20c4d66cf05cf7c4347dc024f2e619a2ee1
Michael <93211335+waveyboym@users.noreply.github.com>
2023-02-12 20:37:12 +0200

    Add files via upload

---
+++ flappy bird files/src/components.rs
@@ -0,0 +1,27 @@
+use sdl2::rect::{Rect, Point};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Direction {
+    Up,
+    Movetoleft,
+    Null,
+}
+
+/// The current position of a given entity
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Position(pub Point);
+
+/// The current speed of a given entity
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Velocity {
+    pub speed: i32,
+    pub direction: Direction,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Sprite {
+    /// The specific spritesheet to render from
+    pub spritesheet: usize,
+    /// The current region of the spritesheet to be rendered
+    pub region: Rect,
+}
